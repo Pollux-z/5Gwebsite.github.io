62c54fc8ad8c41ec0fde504054ba708ed1e89a0a
Pollux-z <64244900+Pollux-z@users.noreply.github.com>
2023-09-22 13:21:06 +0700

    add function change color

--- changeBG.js
+++ changeBG.js
@@ -1,4 +1,30 @@
-// function chBackFontcolor(bg, fn) {
-//     document.body.style.backgroundColor = bg;
-//     document.body.style.color = fn;
-// }
+function cgColors(fc, bg) {
+    const bodyColor = document.body;
+    bodyColor.style.background = bg;
+    bodyColor.style.color = fc;
+    const cardColor = document.querySelectorAll(".card , button, footer, i, ul, h3, h4");
+      for (let i = 0; i < cardColor.length; i++) {
+        cardColor[i].style.backgroundColor = bg;
+        cardColor[i].style.color = fc;
+      }
+      const carouselColor = document.querySelectorAll(".text-stroke, a, p");
+      for (let i = 0; i < carouselColor.length; i++) {
+        carouselColor[i].style.color = fc;
+      }
+      const imgColor = document.querySelectorAll("img");
+      for (let i = 0; i < imgColor.length; i++) {
+        imgColor[i].style.filter = 'grayscale(100%)';
+      }
+      const buttonHover = document.querySelectorAll("button");
+      for (let i = 0; i < buttonHover.length; i++) {
+        buttonHover[i].classList.remove('button');
+        buttonHover[i].classList.add('buttonHover');
+      }
+      
+      
+    //   const buttonColor = document.querySelectorAll("button");
+    //   for (let i = 0; i < buttonColor.length; i++) {
+    //     buttonColor[i].style.backgroundColor = 'black';
+    //     buttonColor[i].style.color = "yellow";
+    //   }
+}
